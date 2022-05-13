fe713d669e27c9b84477b71947f4298c63a96a89
Lucas Ribeiro <lucasseteanos@hotmail.com>
2022-05-13 06:58:48 -0300

    Bug fixes

--- bootstrap_single.sh
+++ bootstrap_single.sh
@@ -12,31 +12,31 @@ if [ -z "${TAG}" ]; then
 fi
 
 # Clone the MultiPool repository if it doesn't exist.
-if [ ! -d $HOME/multipool/yiimp_single ]; then
-	echo Downloading MultiPool YiiMP Single Server Installer ${TAG}. . .
-	git clone \
-		-b ${TAG} --depth 1 \
-		https://github.com/lucasribeirop/multipool_yiimp_single \
-		$HOME/multipool/yiimp_single \
-		< /dev/null 2> /dev/null
-
-	echo
-fi
+#if [ ! -d $HOME/multipool/yiimp_single ]; then
+#	echo Downloading MultiPool YiiMP Single Server Installer ${TAG}. . .
+#	git clone \
+#		-b ${TAG} --depth 1 \
+#		https://github.com/lucasribeirop/multipool_yiimp_single \
+#		$HOME/multipool/yiimp_single \
+#		< /dev/null 2> /dev/null
+#
+#	echo
+#fi
 
 # Change directory to it.
 cd $HOME/multipool/yiimp_single
 
 # Update it.
-sudo chown -R $USER $HOME/multipool/install/.git/
-if [ "${TAG}" != `git describe --tags` ]; then
-	echo Updating MultiPool YiiMP Single Server Installer to ${TAG} . . .
-	git fetch --depth 1 --force --prune origin tag ${TAG}
-	if ! git checkout -q ${TAG}; then
-		echo "Update failed. Did you modify something in `pwd`?"
-		exit
-	fi
-	echo
-fi
+#sudo chown -R $USER $HOME/multipool/install/.git/
+#if [ "${TAG}" != `git describe --tags` ]; then
+#	echo Updating MultiPool YiiMP Single Server Installer to ${TAG} . . .
+#	git fetch --depth 1 --force --prune origin tag ${TAG}
+#	if ! git checkout -q ${TAG}; then
+#		echo "Update failed. Did you modify something in `pwd`?"
+#		exit
+#	fi
+#	echo
+#fi
 
 # Start setup script.
 cd $HOME/multipool/yiimp_single
